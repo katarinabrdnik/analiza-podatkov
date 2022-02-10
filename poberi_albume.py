7551ccc1940b75aad1c8f75ce281341ae6d9f39a
Katarina Brdnik <katarina11brdnik@gmail.com>
2022-02-10 17:43:04 +0100

    noče pobrat html-jev, this is fine

--- poberi_albume.py
+++ poberi_albume.py
@@ -3,7 +3,9 @@ import os.path
 import re
 
 vzorec_bloka = re.compile(
-    r'<div id="pos.*?">\s*</div>\s*</div>',
+    r'<div id="pos\d\d?\d?\d?"'
+    r'.*?'
+    r'class="linkfire_container lazyload">',  
     flags=re.DOTALL
 )
 
@@ -14,16 +16,21 @@ vzorec_albuma = re.compile(
     r'class="artist">(?P<Izvajalec>.*?)</a></div>'
 )
 
+#vzorec_osebe = re.compile(
+#    
+#)
+headers = {'User-Agent': 'My User Agent 1.0'}
+
 def ime_datoteke(st_strani):
     return f"najpopularnejsi-albumi-{st_strani}.html"
 
 for st_strani in range(1, 126):
-    if os.path.isfile("/analiza-podatkov/najpopularnejsi-albumi-{st_strani}.html") == False:
+    if os.path.isfile("/analiza-podatkov/pobrani_html/najpopularnejsi-albumi-{st_strani}.html") == False:
         url = (
             f"https://rateyourmusic.com/charts/popular/album/all-time/exc:live,archival/{st_strani}/#results"
         )
         print(f"Zajemam {url}")
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         vsebina = response.text
         with open(ime_datoteke(st_strani), 'w') as datoteka:
             datoteka.write(vsebina)
